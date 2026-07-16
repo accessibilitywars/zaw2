05f01c8822287841de5ee3086d97e0d30c715012
Andy Berdan <andy@berdan.ca>
2026-07-15 20:50:00 -0400

    Adapted roll_forward to auto-roll unchanged with fulcrum

--- roll_forward.sh
+++ roll_forward.sh
@@ -7,9 +7,8 @@ if [ "$1" == "" ]; then
 	echo "Usage: $0 OLD-PATCH NEW-PATCH"
 fi
 
-echo "Updating $OLD to $NEW..."
+echo "Rolling forward unchanged builds $OLD to $NEW..."
 
-for i in `grep -l "$OLD" content/builds/*.md`; do
-	echo "... $i"
+for i in `gw2fulcrum content/builds/patch/$NEW.txt content/builds/[^_]*\.md`; do
 	sed -i "s/balance = \"$OLD\"/balance = \"$NEW\"/" $i
 done
